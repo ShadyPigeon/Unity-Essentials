581adedc4133499f731e971eca1352a807d31f59
Emmanuel Vaccaro <emmanuel.vaccaro@gmail.com>
2019-10-15 13:40:00 +1100

    Added Package

---
+++ Unity-Essentials/Runtime/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ShadyPigeon.UnityEssentials
+{
+    public class Singleton<TSelfType> : MonoBehaviour where TSelfType : MonoBehaviour
+    {
+        private static TSelfType m_Instance = null;
+        public static TSelfType Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    var type = typeof(TSelfType);
+                    m_Instance = (TSelfType)FindObjectOfType(type);
+                    if (m_Instance == null)
+                    {
+                        m_Instance = (new GameObject(type.Name)).AddComponent<TSelfType>();
+                    }
+                    DontDestroyOnLoad(m_Instance);
+                }
+                return m_Instance;
+            }
+        }
+    }
+}
