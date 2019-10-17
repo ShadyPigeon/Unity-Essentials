79b3d84e73455ae8a5c336361c20c90b9c785ffb
Emmanuel Vaccaro <emmanuel.vaccaro@gmail.com>
2019-10-17 18:45:25 +1100

    Update ArrayListExtension.cs

--- Runtime/Extensions/ArrayListExtension.cs
+++ Runtime/Extensions/ArrayListExtension.cs
@@ -35,7 +35,7 @@ namespace UnityEssentials
         public static ArrayList Distinct(this ArrayList collection)
         {
             ArrayList result = new ArrayList();
-            foreach (object entity in collection)
+            foreach (object entityityE in collection)
             {
                 if (!result.Contains(entity))
                 {
