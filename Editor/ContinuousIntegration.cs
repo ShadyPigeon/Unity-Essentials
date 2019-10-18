c2b7ff01ba3e2bb6ba3cf10aff93026fb3ae5dc1
Emmanuel Vaccaro <emmanuel.vaccaro@gmail.com>
2019-10-18 19:26:30 +1100

    Added Continuous Integration

---
+++ Editor/ContinuousIntegration.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor.Build.Reporting;
+using UnityEditor;
+
+public class ContinuousIntegration
+{
+    static string[] scenes = FindEnabledEditorScenes();
+        
+    static string GetAppName()
+    {
+        return Application.productName;
+    }
+
+    static string GetTargetName(BuildTargetGroup targetGroup)
+    {
+        return targetGroup.ToString();
+    }
+
+    public static void PerformBuild()
+    {
+        PerformAndroidBuild();
+
+    }
+
+    [MenuItem("Custom/CI/Android")]
+    static void PerformAndroidBuild()
+    {
+        string targetDir = GetAppName() + ".apk";
+        BuildTargetGroup targetGroup = BuildTargetGroup.Android;
+        BuildTarget target = BuildTarget.Android;
+        BuildOptions options = BuildOptions.None;
+        GenericBuild(scenes, GetTargetName(targetGroup) + "/" + targetDir, targetGroup, target, options);
+    }
+
+    [MenuItem("Custom/CI/Build Mac OS X")]
+    static void PerformMacOSXBuild()
+    { 
+        string targetDir = GetAppName() + ".app";
+        BuildTargetGroup targetGroup = BuildTargetGroup.Standalone;
+        BuildTarget target = BuildTarget.StandaloneOSX;
+        BuildOptions options = BuildOptions.None;
+        GenericBuild(scenes, GetTargetName(targetGroup) + "/" + targetDir, targetGroup, target, options);
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string targetDir, BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+        BuildReport buildReport = BuildPipeline.BuildPlayer(scenes, "Builds" + "/" + targetDir, buildTarget, buildOptions);
+        BuildSummary summary = buildReport.summary;
+        switch (summary.result)
+        {
+            case BuildResult.Unknown:
+                break;
+            case BuildResult.Succeeded:
+                break;
+            case BuildResult.Failed:
+                throw new System.Exception("BuildPlayer failure: " + "UNSURE");
+            case BuildResult.Cancelled:
+                break;
+            default:
+                break;
+        }
+    }
+}
